3280444657b3bc71152c098612a9510c0bebcfb8
Stefan hegny <hegny@hydrografix.com>
2023-05-11 22:13:49 +0200

    html: trying to improve white balance

--- helpstuff/mydng.c
+++ helpstuff/mydng.c
@@ -19,11 +19,11 @@ int main (int argc, char **argv)
 	// this color matrix is definitely inaccurate, TODO: calibrate
 	static const float cam_xyz[] = {
 		// R 	G     	B
-		1.000,	0.000,	0.000,	// R
+		1.400,	0.000,	0.000,	// R
 		0.000,	1.000,	0.000,	// G
-		0.000,	0.000,	1.000	// B
+		0.000,	0.000,	1.400	// B
 	};
-	static const float neutral[] = { 1.0, 1.0, 1.0 }; // TODO calibrate
+	static const float neutral[] = { 0.6, 1.0, 0.6 }; // TODO calibrate
 	long white=0x0ff;
 
 	int status=1, row;
