f33ade570ca347826d7d27006fa3cf4d28029a59
Stefan hegny <hegny@hydrografix.com>
2024-04-26 09:39:03 +0200

    V3.7.5_f85063c

--- imbraw2dng.js
+++ imbraw2dng.js
@@ -595,7 +595,7 @@ handleone(fx) {
 /* *************************************** Main class *************************************** */
 class ImBCBase {
 /* Indentation out */
-static version = "V3.7.4_dev"; // actually const
+static version = "V3.7.5_f85063c"; // actually const
 static alllangs = [ 'de' , 'en', 'fr', 'ru', 'ja', '00' ]; // actually const
 static texts = { // actually const
 	langs: { de: 'DE', en: 'EN', fr: 'FR' , ru: 'RU', ja: 'JA' },
