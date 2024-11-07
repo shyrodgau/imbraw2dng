d01c2994e5f4c8460a55af420dfd64169e5535e6
Stefan Hegny (hydrografix Consulting GmbH) <hegny@hydrografix.com>
2024-11-07 16:35:25 +0100

    add automated app tests

---
+++ test/test_app.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node 
+/* Appium tests */
+
+var fs = require('fs');
+
+
+const TESTDAT='/home/hegny/prog/imbraw2dng/samples/webroot';
+const TESTDAT0='/home/hegny/prog/imbraw2dng/samples';
+
+const TESTURL='http://127.0.0.1:8889/';
+
+const FILELIST=[ '2024_0217_121752_001.dng', '2029_0710_010203_001.dng', '2024_0217_121754_002.JPG', '2029_0707_120426_021.dng' ];
+
+// test executable path:
+const TESTEXES='/home/hegny/prog/imbraw2dng/github';
+
+// workdir (this here):
+const TESTWORK='/home/hegny/prog/imbraw2dng/github/test';
+
+const {remote} = require('webdriverio');
+
+const capabilities = {
+  'platformName': 'Android',
+  'appium:automationName': 'UiAutomator2',
+  'appium:deviceName': 'Android',
+  'appium:appPackage': 'eu.imback.os.app',
+  'appium:appActivity': '.MainActivity',
+  'appium:autoWebview': true,
+  'appium:newCommandTimeout': 320
+};
+
+const wdOpts = {
+  hostname: process.env.APPIUM_HOST || '127.0.0.1',
+  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+  logLevel: 'silent',
+  capabilities,
+};
+//  logLevel: 'info',
+
+async function ffile(driver, idx, cb) {
+	const ff = await driver.executeScript('mobile: pullFile', [{remotePath: '/storage/emulated/0/DCIM/nn/' + FILELIST[idx]}]);
+	fs.writeFile('/home/hegny/Downloads/appium_' + FILELIST[idx] + '.b64', ff, {encoding: null, flush: true, flag: 'w' }, async (e) => {
+    	if (e) console.log(e?.toString());
+    	if (idx < FILELIST.length - 1) {
+    		ffile(driver, idx + 1, cb);
+    	}
+    	else if (cb) cb();
+    });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function runTest() {
+  const driver = await remote(wdOpts);
+  try {
+  	// empty the dir, first add a file so it is not empty
+  	driver.executeScript('mobile: pushFile', [{remotePath: '/storage/emulated/0/DCIM/nn/x', payload: 'bml4Cg=='}]);
+  	driver.executeScript('mobile: shell', [{command:'rm', args: [ '/storage/emulated/0/DCIM/nn/*' ]}]);
+	await driver.pause(5000);
+    const batteryItem = await driver.$('#tobrows');
+    await batteryItem.click();
+	await driver.pause(2000);
+    const imgrp = await driver.$('#SELC_2029_07');
+    await imgrp.click();
+    const imgrp2 = await driver.$('#SELC_2024_02_17');
+    await imgrp2.click();
+	await driver.pause(2000);
+	const sbytype = await driver.$('#sbytype');
+	await sbytype.click();
+	await driver.pause(7000);
+	/*const mp4s = await driver.$('#SELC_oth2024_02');
+	await mp4s.click();*/
+    const selbut = await driver.$('#doselbut');
+    await selbut.click();
+	await driver.pause(7000);
+	//const ctxx = await driver.getContexts();
+	//console.log(JSON.stringify(ctxx));
+	//await driver.context('NATIVE_APP');
+    const logbut = await driver.$('#dlprogresslogbtn');
+    while (1) {
+		const cliki = await logbut.getAttribute('disabled')
+		//console.log('CLICK *** ' + cliki + '*** CLIC ');
+		if (!cliki) break;
+		await sleep(500);
+	}
+	//await driver.pause(27000);
+    //const fold = await driver.$('=USE THIS FOLDER');
+    await logbut.click();
+	await driver.pause(1000);
+    const msglog = await driver.$('#outmsg');
+    const txt = await msglog.getText();
+    console.log(txt);
+    let proc = true;
+    let fileidx = 0;
+    await fs.writeFile('/home/hegny/Downloads/appium.log', txt, {encoding: null, flush: true, flag: 'w' }, async (e) => {
+    	if (e) console.log(e?.toString());
+		await ffile(driver, 0, async () => {
+			//await driver.deleteSession();
+			proc = false;
+		});
+    });
+    while (proc) await sleep(1000);
+  } finally {
+    await driver.pause(1000);
+    await driver.deleteSession();
+  }
+}
+
+runTest().catch(console.error);
+
--- test/testwrapper.sh
+++ test/testwrapper.sh
@@ -34,6 +34,29 @@ mkdir -p ${TESTWORK}/outdir/html
 find ~/Downloads -newer ~/Downloads/imbraw2dng_test_${testid}_startmark -type f ! -name imbraw2dng_test_${testid}_endmark -exec mv -v {} ${TESTWORK}/outdir/html \; 2>&1 | tee -a $log
 rm ~/Downloads/imbraw2dng_test_${testid}_endmark ~/Downloads/imbraw2dng_test_${testid}_startmark
 
+touch ~/Downloads/imbraw2dng_test_${testid}_startmark
+( python3 -m http.server 8080 > ${log}_http 2>&1 )&
+ps -ef|grep -q 'appiu[m]'
+if [ $? -ne 0 ]; then
+	( PATH="/opt/google/android-studio/cmdline-tools/latest/bin/:/opt/google/android-studio/emulator/:$PATH" ANDROID_SDK_ROOT=/opt/google/android-studio ANDROID_HOME=/opt/google/android-studio appium \
+		server -a 127.0.0.1 --allow-insecure chromedriver_autodownload --allow-insecure adb_shell  > /dev/null 2>&1 ) &
+	sleep 6
+fi
+
+
+${TESTWORK}/test_app.js 2>&1 | tee -a $log 2>&1
+sleep 3
+touch ~/Downloads/imbraw2dng_test_${testid}_endmark
+
+mkdir -p ${TESTWORK}/outdir/app
+find ~/Downloads -newer ~/Downloads/imbraw2dng_test_${testid}_startmark -type f ! -name imbraw2dng_test_${testid}_endmark -exec mv -v {} ${TESTWORK}/outdir/app \; 2>&1 | tee -a $log
+rm ~/Downloads/imbraw2dng_test_${testid}_endmark ~/Downloads/imbraw2dng_test_${testid}_startmark
+
+for f in ${TESTWORK}/outdir/app/*.b64; do openssl base64 -A -d -in $f > ${f%%.b64} && rm $f; done
+echo Appium Log >> $log
+cat ${TESTWORK}/outdir/app/appium.log >> $log 2>&1
+echo >> $log
+
 kk=1
 for f in $( ls ${TESTWORK}/outdir/html/imb*zip | sort ); do
 	nn=$( basename "$f" | sed 's/\(imb[^_]*_\)[0-9]*_/\1'$kk'_/g' )
@@ -60,20 +83,24 @@ find . -name \*.zip -type f -print -exec unzip -v {} \; -exec unzip -t {} \; 2>&
 
 echo 'LINT TESTS' | tee -a $log
 ln -sf ${TESTEXES}/imbraw2dng.js ih.mjs
+echo imbraw2dng.js | tee -a $log
 node /home/hegny/prog/imbraw2dng/github/node_modules/eslint/bin/eslint.js -c ${TESTEXES}/eslint.config-node.mjs ih.mjs 2>&1 | tee -a $log
 stl=$( grep -n '<script' ${TESTEXES}/imbraw2dng.html|cut -d: -f1 )
 endl=$( grep -n '</script' ${TESTEXES}/imbraw2dng.html|cut -d: -f1 )
 head -$(( $endl - 1 )) ${TESTEXES}/imbraw2dng.html|tail -$(( $endl - $stl - 1 )) > ih.js
+echo imbraw2dng.html | tee -a $log
 node /home/hegny/prog/imbraw2dng/github/node_modules/eslint/bin/eslint.js -c ${TESTEXES}/eslint.config.mjs  ih.js 2>&1 | tee -a $log
 
 stla=$( grep -n '<script' ${TESTEXES}/imbapp.htm|cut -d: -f1 | head -1 )
 endla=$( grep -n '</script' ${TESTEXES}/imbapp.htm|cut -d: -f1 |head -1)
 head -$(( $endla - 1 )) ${TESTEXES}/imbapp.htm|tail -$(( $endla - $stla - 1 )) > iaa.js
+echo imbapp.html | tee -a $log
 node /home/hegny/prog/imbraw2dng/github/node_modules/eslint/bin/eslint.js -c ${TESTEXES}/eslint.config.mjs  iaa.js 2>&1 | tee -a $log
 
 stlb=$( grep -n '<script' ${TESTEXES}/imbapp.htm|cut -d: -f1 | tail -1 )
 endlb=$( grep -n '</script' ${TESTEXES}/imbapp.htm|cut -d: -f1 |tail -1)
 head -$(( $endlb - 1 )) ${TESTEXES}/imbapp.htm|tail -$(( $endlb - $stlb - 1 )) > iaw.js
+echo imbapp.html worker | tee -a $log
 node /home/hegny/prog/imbraw2dng/github/node_modules/eslint/bin/eslint.js -c ${TESTEXES}/eslint.config.mjs  iaw.js 2>&1 | tee -a $log
 ls -l ia*.js
 
@@ -86,3 +113,6 @@ done
 #rm -rf *_${$}_tmp 2>&1 | tee -a $log 2>&1
 
 kill $webid 2>&1 | tee -a $log 2>&1
+kill %1
+kill %2
+kill %3
