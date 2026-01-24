4590d8b2dbc4706d2ba93d6d00ab5ed750072d00
Stefan Hegny (hydrografix Consulting GmbH) <hegny@hydrografix.com>
2026-01-24 12:30:36 +0100

    V6.6.2_0e97efa

--- imbraw2dng.js
+++ imbraw2dng.js
@@ -849,7 +849,7 @@ static readinta(arr, off) {
 const globals = {
 debugflag: false,
 /* Indentation out - globals */
-version: "V6.6.2_609e81a", // actually const // VERSION EYECATCHER
+version: "V6.6.2_0e97efa", // actually const // VERSION EYECATCHER
 alllangs: [ 'de' , 'en', 'ja', '00' /*, 'fr', 'ru'*/ ], // actually const
 // generic user input timestamp always complete
 //               y     y    y    y      .       m    m     .       d     d      .       h    h      .       m    m      .       s    s
