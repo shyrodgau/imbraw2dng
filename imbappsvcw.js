f85bbe72ebba79f9b7b2a1ac81595c8d26f6b79f
Stefan Hegny (hydrografix Consulting GmbH) <hegny@hydrografix.com>
2026-02-15 12:59:14 +0100

    V6.7.9_b3aae1f

--- imbappsvcw.js
+++ imbappsvcw.js
@@ -1,5 +1,5 @@
 /* imbapp caching service worker for offline */
-const curcache = "v1"; // next 1
+const curcache = "v2"; // next 1
 const addResourcesToCache = async (resources) => {
   const cache = await caches.open(curcache);
   await cache.addAll(resources);
